d806cf029036385622486413613d712fa52f90df
Williamlin <35906798+williamlin910403@users.noreply.github.com>
2021-04-10 08:46:20 +0800

    Tiny change
    Chain id is wrong

--- frontend/src/components/Dapp.js
+++ frontend/src/components/Dapp.js
@@ -25,7 +25,7 @@ import Axios from "axios"; // Import Axios or use Fetch.
 // This is the Hardhat Network id, you might change it in the hardhat.config.js
 // Here's a list of network ids https://docs.metamask.io/guide/ethereum-provider.html#properties
 // to use when deploying to other networks.
-const HARDHAT_NETWORK_ID = '31337';
+const HARDHAT_NETWORK_ID = '1337';
 
 // This is an error code that indicates that the user canceled a transaction
 const ERROR_CODE_TX_REJECTED_BY_USER = 4001;
